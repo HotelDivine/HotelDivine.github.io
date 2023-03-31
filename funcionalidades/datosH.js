7f2890f998ffb8a22788cb7dec82f546363bb0f3
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-03-31 18:55:31 -0300

    agregue la funcionalidad mostrar

--- funcionalidades/datosH.js
+++
@@ -1,5 +0,0 @@
-let fs = require('fs');
-let dato = fs.readFileSync('./habitaciones.json', 'utf8');
-let objJson = JSON.parse(dato)
-
-module.exports = objJson;
--- funcionalidades/habitaciones.js
+++ funcionalidades/habitaciones.js
@@ -1,4 +1,4 @@
-			let json = [
+let json = [
     {
         "id": 1,
         "ambientes": 2,
@@ -16,7 +16,7 @@
         "ambientes": 9,
         "precio": 7000,
         "reservada": false
-        },
+    },
     {
         "id": 4,
         "ambientes": 1,
@@ -48,16 +48,57 @@
         "reservada": false
     }
 ];
-				for (var response in json){
-					let datos = json[response];
-					let id = datos["id"];
-					let ambientes = datos["ambientes"];
-					let precio = datos["precio"];
-					let reservada = datos["reservada"];
+for (var response in json) {
+    let datos = json[response];
+    let id = datos["id"];
+    let ambientes = datos["ambientes"];
+    let precio = datos["precio"];
+    let reservada = datos["reservada"];
+    // Crear el DOM
 
+    function mostrarHabitaciones() {
+        /*
+                console.log(`Numero de la habitación: ${id}`);
+                console.log(`Ambientes: ${ambientes}`);
+                console.log(`Precio por noche: ${precio}`);
+                console.log(`Disponibilidad: ${reservada ? "Sí" : "No"}`);
+                console.log("--------------------");
+        */
+        const id = document.createElement("div");
+        div.innerHTML(id);
+        document.rooms.appendChild(id);
+        const amb = document.createElement("div");
+        div.innerHTML(ambientes);
+        document.rooms.appendChild(amb);
+        const pre = document.createElement("div");
+        div.innerHTML(precio);
+        document.rooms.appendChild(pre);
+        const res = document.createElement("div");
+        div.innerHTML(reservada);
+        document.rooms.appendChild(res);
+    }
+    //mostrarHabitaciones();
+}
 
-					// Crear el DOM
-                    // console.log(ambientes);
-
-				}
-					console.log(datos);
+/*
+function reservarHabitacion(idBuscar) {
+    let encontro = buscarHabitacion(idBuscar);
+    if (encontro.id == idBuscar) {
+        encontro.reservada = !encontro.reservada;
+        return encontro;
+    } else {
+        return "habitacioncleta no encontrada";
+    }
+}
+function habitacionParaLaReserva() {
+    return habitaciones.filter((habitacion) => habitacion.reservada === false);
+}
+function totalDeReservas() {
+    let total = habitaciones.filter((habitacion) => habitacion.reservada === true);
+    let depo = total.reduce((acum, num) => {
+        acum.push(num.precio);
+        return acum
+    }, []);
+    return depo.reduce((acum, num) => acum + num);
+}
+*/
