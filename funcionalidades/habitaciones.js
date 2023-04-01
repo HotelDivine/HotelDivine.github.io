1bc2577be8dc9a93f912e83b7ecea1dae0e954c8
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-04-01 13:27:39 -0300

    tp - habitaciones - edit - funcion mostrarHabitaciones funcionando

--- funcionalidades/habitaciones.js
+++ funcionalidades/habitaciones.js
@@ -1,100 +1,106 @@
 let json = [
     {
-        "id": 1,
+        "Numero": 1,
         "ambientes": 2,
         "precio": 1500,
-        "reservada": true
+        "Vacante": true
     },
     {
-        "id": 2,
+        "Numero": 2,
         "ambientes": 6,
         "precio": 3000,
-        "reservada": false
+        "Vacante": false
     },
     {
-        "id": 3,
+        "Numero": 3,
         "ambientes": 9,
         "precio": 7000,
-        "reservada": false
+        "Vacante": false
     },
     {
-        "id": 4,
+        "Numero": 4,
         "ambientes": 1,
         "precio": 250,
-        "reservada": true
+        "Vacante": true
     },
     {
-        "id": 5,
+        "Numero": 5,
         "ambientes": 8,
         "precio": 170,
-        "reservada": false
+        "Vacante": false
     },
     {
-        "id": 6,
+        "Numero": 6,
         "ambientes": 5,
         "precio": 220,
-        "reservada": false
+        "Vacante": false
     },
     {
-        "id": 7,
+        "Numero": 7,
         "ambientes": 7,
         "precio": 190,
-        "reservada": false
+        "Vacante": false
     },
     {
-        "id": 8,
+        "Numero": 8,
         "ambientes": 4,
         "precio": 160,
-        "reservada": false
+        "Vacante": false
     }
 ];
 for (var response in json) {
     let datos = json[response];
-    let id = datos["id"];
+    let Numero = datos["Numero"];
     let ambientes = datos["ambientes"];
     let precio = datos["precio"];
-    let reservada = datos["reservada"];
+    let Vacante = datos["Vacante"];
     // Crear el DOM
 
-    function mostrarHabitaciones() {
-        /*
-                console.log(`Numero de la habitación: ${id}`);
-                console.log(`Ambientes: ${ambientes}`);
-                console.log(`Precio por noche: ${precio}`);
-                console.log(`Disponibilidad: ${reservada ? "Sí" : "No"}`);
-                console.log("--------------------");
-        */
-        const id = document.createElement("div");
-        div.innerHTML(id);
-        document.rooms.appendChild(id);
-        const amb = document.createElement("div");
-        div.innerHTML(ambientes);
-        document.rooms.appendChild(amb);
-        const pre = document.createElement("div");
-        div.innerHTML(precio);
-        document.rooms.appendChild(pre);
-        const res = document.createElement("div");
-        div.innerHTML(reservada);
-        document.rooms.appendChild(res);
+    function mostrarHabitaciones(habitacion) {
+        let habitacionHTML = "<ul>";
+        for (let propiedad in habitacion) {
+            habitacionHTML += "<li>" + propiedad + ": " + habitacion[propiedad] + "</li>";
+        }
+        habitacionHTML += "</ul>";
+        document.getElementById("habitaciones").innerHTML += habitacionHTML;
     }
-    //mostrarHabitaciones();
+
+    mostrarHabitaciones(datos);
 }
+    /*
+console.log(`Numero de la habitación: ${id}`);
+console.log(`Ambientes: ${ambientes}`);
+console.log(`Precio por noche: ${precio}`);
+console.log(`Disponibilidad: ${Vacante ? "Sí" : "No"}`);
+console.log("--------------------");
+const amb = document.createElement("p");
+p.innerHTML(ambientes);
+document.rooms.appendChild(amb);
+const pre = document.createElement("div");
+div.innerHTML(precio);
+document.rooms.appendChild(pre);
+const res = document.createElement("div");
+div.innerHTML(Vacante);
+document.rooms.appendChild(res);
+}
+*/
+    //mostrarHabitaciones();
 
 /*
 function reservarHabitacion(idBuscar) {
     let encontro = buscarHabitacion(idBuscar);
     if (encontro.id == idBuscar) {
-        encontro.reservada = !encontro.reservada;
+        encontro.Vacante = !encontro.Vacante;
         return encontro;
     } else {
         return "habitacioncleta no encontrada";
     }
 }
 function habitacionParaLaReserva() {
-    return habitaciones.filter((habitacion) => habitacion.reservada === false);
+    return habitaciones.filter((habitacion) => habitacion.Vacante === false);
 }
 function totalDeReservas() {
-    let total = habitaciones.filter((habitacion) => habitacion.reservada === true);
+    let total = habitaciones.filter((habitacion) => habitacion.Vacante === true);
     let depo = total.reduce((acum, num) => {
         acum.push(num.precio);
         return acum
