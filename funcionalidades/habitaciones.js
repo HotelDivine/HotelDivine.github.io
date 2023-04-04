193fe6847ca1531d76d466dbcf2b6e80e9217eaf
Nando Bravo <nicnando123@gmail.com>
2023-04-04 20:25:59 -0300

    tp - habitaciones - add&edit - hice la impresion mas comoda de leer para los desarrolladores y agregue estilos

--- funcionalidades/habitaciones.js
+++ funcionalidades/habitaciones.js
@@ -1,115 +1,181 @@
+let d = document;
+
 let json = [
     {
-        "Numero": "VISTA AL MAR",
-        "Description":"Habitación doble con vista al mar y balcón privado. Esta habitación es perfecta para parejas que buscan una escapada romántica. Disfruta de las impresionantes vistas al mar desde tu balcón privado mientras tomas una copa de vino",
+        "id":1,
+        "titulo": "VISTA AL MAR",
+        "descripcion":"Habitación doble con vista al mar y balcón privado. Esta habitación es perfecta para parejas que buscan una escapada romántica. Disfruta de las impresionantes vistas al mar desde tu balcón privado mientras tomas una copa de vino",
         "ambientes": 2,
         "precio": 1500,
-        "Vacante": true
+        "vacante": true,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "INDIVIDUAL",
-        "Description":"Habitación individual con cama king size y baño privado. Esta habitación es ideal para viajeros solitarios que buscan comodidad y privacidad. Descansa en la cama king size después de un largo día de turismo y relájate en tu baño privado.",
+        "id":2,
+        "titulo": "INDIVIDUAL",
+        "descripcion":"Habitación individual con cama king size y baño privado. Esta habitación es ideal para viajeros solitarios que buscan comodidad y privacidad. Descansa en la cama king size después de un largo día de turismo y relájate en tu baño privado.",
         "ambientes": 6,
         "precio": 3000,
-        "Vacante": false
+        "vacante": false,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "SUITE",
-        "Description":"Suite junior con sala de estar y vistas panorámicas a la ciudad. Esta suite es perfecta para aquellos que buscan un poco más de espacio y comodidad. Disfruta de las vistas panorámicas a la ciudad desde tu sala de estar mientras te relajas en el sofá.",
+        "id":3,
+        "titulo": "SUITE",
+        "descripcion":"Suite junior con sala de estar y vistas panorámicas a la ciudad. Esta suite es perfecta para aquellos que buscan un poco más de espacio y comodidad. Disfruta de las vistas panorámicas a la ciudad desde tu sala de estar mientras te relajas en el sofá.",
         "ambientes": 9,
         "precio": 7000,
-        "Vacante": false
+        "vacante": false,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "FAMILIAR",
-        "Description":"Habitación familiar con dos camas dobles y cocina americana. Esta habitación es ideal para familias que buscan comodidad y espacio. Prepara tus propias comidas en la cocina americana y descansa en las cómodas camas dobles después de un día lleno de diversión.",
+        "id":4,
+        "titulo": "FAMILIAR",
+        "descripcion":"Habitación familiar con dos camas dobles y cocina americana. Esta habitación es ideal para familias que buscan comodidad y espacio. Prepara tus propias comidas en la cocina americana y descansa en las cómodas camas dobles después de un día lleno de diversión.",
         "ambientes": 1,
         "precio": 250,
-        "Vacante": true
+        "vacante": true,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "EJECUTIVA",
-        "Description":"Habitación ejecutiva con escritorio y vistas a la montaña. Esta habitación es perfecta para viajeros de negocios que necesitan trabajar durante su estancia. Disfruta de las impresionantes vistas a la montaña mientras trabajas en tu escritorio.",
+        "id":5,
+        "titulo": "EJECUTIVA",
+        "descripcion":"Habitación ejecutiva con escritorio y vistas a la montaña. Esta habitación es perfecta para viajeros de negocios que necesitan trabajar durante su estancia. Disfruta de las impresionantes vistas a la montaña mientras trabajas en tu escritorio.",
         "ambientes": 8,
         "precio": 170,
-        "Vacante": false
+        "vacante": false,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "LUJOSA",
-        "Description":"Habitación superior con cama queen size y bañera de hidromasaje. Esta habitación es perfecta para aquellos que buscan un poco más de lujo durante su estancia. Relájate en la bañera de hidromasaje después de un largo día y descansa en la cómoda cama queen size.",
+        "id":6,
+        "titulo": "LUJOSA",
+        "descripcion":"Habitación superior con cama queen size y bañera de hidromasaje. Esta habitación es perfecta para aquellos que buscan un poco más de lujo durante su estancia. Relájate en la bañera de hidromasaje después de un largo día y descansa en la cómoda cama queen size.",
         "ambientes": 5,
         "precio": 220,
-        "Vacante": false
+        "vacante": false,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "ESTANDAR",
-        "Description":"Habitación estándar con dos camas individuales y vistas al jardín. Esta habitación es ideal para aquellos que buscan una estancia cómoda y sencilla. Disfruta de las vistas al jardín mientras descansas en las cómodas camas individuales.",
+        "id":7,
+        "titulo": "ESTANDAR",
+        "descripcion":"Habitación estándar con dos camas individuales y vistas al jardín. Esta habitación es ideal para aquellos que buscan una estancia cómoda y sencilla. Disfruta de las vistas al jardín mientras descansas en las cómodas camas individuales.",
         "ambientes": 7,
         "precio": 190,
-        "Vacante": false
+        "vacante": false,
+        "top": "Pareja",
+        "calificacion": 4.2
     },
     {
-        "Numero": "SUITE PRESIDENCIAL",
-        "Description":"Suite presidencial con sala de estar, comedor y terraza privada. Esta suite es perfecta para aquellos que buscan lo mejor durante su estancia. Disfruta de la amplia sala de estar, el comedor y la terraza privada mientras te relajas en el lujo.",
+        "id":8,
+        "titulo": "SUITE PRESIDENCIAL",
+        "descripcion":"Suite presidencial con sala de estar, comedor y terraza privada. Esta suite es perfecta para aquellos que buscan lo mejor durante su estancia. Disfruta de la amplia sala de estar, el comedor y la terraza privada mientras te relajas en el lujo.",
         "ambientes": 4,
         "precio": 160,
-        "Vacante": false
+        "vacante": false,
+        "top": "Pareja",
+        "calificacion": 4.2
     }
 ];
+    let contenedor = document.getElementById('all-rooms');
 for (var response in json) {
-    let datos = json[response];
-    let Numero = datos["Numero"];
-    let Description = datos["Description"];
-    let ambientes = datos["ambientes"];
-    let precio = datos["precio"];
-    let Vacante = datos["Vacante"];
+    let data = json[response];
+    let title = data["titulo"];
+    let description = data["descripcion"];
+    let ambientes = data["ambientes"];
+    let price = data["precio"];
+    let vacante = data["vacante"];
+
+    let calificacion = data["calificacion"];
+    let top = data["top"];
+
+
     // Crear el DOM
+    let room = document.createElement('div');
+    room.classList.add('room','col-md-12','pad-0');
+    room.innerHTML = `<div class="room inside d-flex justify-content-between p-4 container-xl">
+                            <div class="room-media col-md-5">
+                                <div class="room-media-img object-fit-cover justify-content-start">
+                                    <img src="images/rooms/gallery-1-1.jpg" class="w-100 h-100 object-fit-cover" id="room-image">
+                                    <div class="calification">
+                                        <p>
+                                            <span id="calification">${calificacion}</span>
+                                            <i class="fas fa-star"></i></p>
+                                    </div>
 
-    function mostrarHabitaciones(habitacion) {
-        let div = document.createElement("div");
-        div.className = "room-content col-md-6 justify-content-between d-flex flex-column";
-        let h3 = document.createElement("h3");
-        h3.innerHTML = Numero;
-        div.appendChild(h3);
+                                </div>
+                            </div>
+                            <div class="room-content col-md-6 justify-content-between d-flex flex-column">
+                                <h3 class="title" id="title">${title}</h3>
+                                <p class="description" id="description">${description}</p>
+                                <div class="room-features justify-content-between d-flex" id="room-reatures">
 
-        let p = document.createElement("p");
-        p.id = "description";
-        p.innerHTML = Description;
-        div.appendChild(p);
+                                    <div class="justify-content-between d-flex align-items-center prices mb-16">
+                                        <i class="fa-solid fa-bookmark"></i>&nbsp;
+                                        <span id="top">Top ${top}</span>
+                                    </div>
 
-        let roomFeaturesDiv = document.createElement("div");
-        roomFeaturesDiv.className = "room-features justify-content-between";
-        roomFeaturesDiv.id = "room-reatures";
+                                    <div class="justify-content-between d-flex align-items-center prices mb-16">
+                                        <p>Desde* </p>&nbsp;
+                                        <p id="currency" class="price">$</p>
+                                        <span id="price" class="price">${price}</span>
+                                    </div>
+                                </div>
+                                <button class="btn btn-success btn-reservation">Iniciar Reserva</button>
+                            </div>
+                        </div>`;
+    contenedor.appendChild(room);
 
-        let p2 = document.createElement("p");
-        let i = document.createElement("i");
-        i.className = "fas fa-star";
-        p2.appendChild(i);
+    // function mostrarHabitaciones(habitacion) {
+    //     let div = document.createElement("div");
+    //     div.className = "room-content col-md-6 justify-content-between d-flex flex-column";
+    //     let h3 = document.createElement("h3");
+    //     h3.innerHTML = Numero;
+    //     div.appendChild(h3);
 
-        let span1 = document.createElement("span");
-        span1.id = "top";
-        span1.innerHTML = "Top Parejas";
-        p2.appendChild(span1);
+    //     let p = document.createElement("p");
+    //     p.id = "description";
+    //     p.innerHTML = Description;
+    //     div.appendChild(p);
 
-        roomFeaturesDiv.appendChild(p2);
+    //     let roomFeaturesDiv = document.createElement("div");
+    //     roomFeaturesDiv.className = "room-features justify-content-between";
+    //     roomFeaturesDiv.id = "room-reatures";
 
-        let h4 = document.createElement("h4");
-        h4.innerHTML = "Desde* $" + precio;
+    //     let p2 = document.createElement("p");
+    //     let i = document.createElement("i");
+    //     i.className = "fas fa-star";
+    //     p2.appendChild(i);
 
-        roomFeaturesDiv.appendChild(h4);
+    //     let span1 = document.createElement("span");
+    //     span1.id = "top";
+    //     span1.innerHTML = "Top Parejas";
+    //     p2.appendChild(span1);
 
-        div.appendChild(roomFeaturesDiv);
+    //     roomFeaturesDiv.appendChild(p2);
 
-        let boton = document.createElement("button");
-        boton.className = "btn -btnsuccess btn-reservation";
-        boton.innerHTML = "Iniciar Reserva";
+    //     let h4 = document.createElement("h4");
+    //     h4.innerHTML = "Desde* $" + precio;
 
-        div.appendChild(boton);
+    //     roomFeaturesDiv.appendChild(h4);
 
-        document.getElementById("room").appendChild(div);
-    }
+    //     div.appendChild(roomFeaturesDiv);
 
-    mostrarHabitaciones(datos);
+    //     let boton = document.createElement("button");
+    //     boton.className = "btn -btnsuccess btn-reservation";
+    //     boton.innerHTML = "Iniciar Reserva";
+
+    //     div.appendChild(boton);
+
+    //     document.getElementById("room").appendChild(div);
+    // }
+
+    // mostrarHabitaciones(datos);
 }
 
 /*
