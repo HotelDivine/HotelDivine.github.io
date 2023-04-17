9f684954b3f8233e8abae77960d8bcc9a2963c79
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-04-17 12:34:18 -0300

    falta borrar

---
+++ js/paises.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+	// Hacer una solicitud a la API
+	fetch('https://restcountries.com/v3.1/all')
+	  .then(response => response.json())
+	  .then(json => {
+	    // Obtener el elemento SELECT del DOM
+	    const select = document.getElementById('inputState');
+
+	    // Iterar a través de los datos de los países
+	    json.forEach(pais => {
+	      // Crear una opción para el país
+	      const option = document.createElement('option');
+	      option.text = pais.name.common;
+	      option.value = pais.name.common;
+
+	      select.appendChild(option);
+	    });
+	  });
+})
