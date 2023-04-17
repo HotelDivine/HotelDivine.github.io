d3d694c01016065159d5df427519a433cdb34ae5
DiegoMoyan0 <diegomoyano02@gmail.com>
2023-04-16 21:38:41 -0300

    archivos de api agregados

--- js/datos-modal.js
+++ js/datos-modal.js
@@ -1,18 +1,53 @@
-const btnSiguiente = document.querySelector('#exampleModalToggle2 .btn.btn-gold');
-btnSiguiente.addEventListener('click', function (event) {
-    event.preventDefault();
-    const inputNombre = document.getElementById('nombre');
-    const inputApellido = document.getElementById('apellido');
-    const inputEmail = document.getElementById('inputEmail3');
-    const inputPais = document.getElementById('inputState');
-    const nombre = inputNombre.value.trim();
-    const apellido = inputApellido.value.trim();
-    const pais = inputPais.value.trim();
-    const email = inputEmail.value.trim();
+document.addEventListener('DOMContentLoaded', function() {
 
-    if (nombre == "" || apellido == "" || pais == "Seleccionar..." || email == "") {
-        alert('Por favor, complete todos los campos.');
-        return;
-    }
-    // agregar código para enviar el formulario si todos los campos son válidos
+      let buttons = document.getElementsByClassName('btn-reservation');
+      for (let i = 0; i < buttons.length; i++) {
+          console.log("i");
+        buttons[i].addEventListener('click', function() {
+          // let btnId = buttons[i];
+
+          // fetch('json/habitaciones.json')
+          //   .then((response) => response.json())
+          //   .then((json) => {
+                
+          //       let precio = json[btnId]["precio"];
+          //       let total = document.getElementById("precio");
+          //       total.innerHTML = precio;
+
+
+          //   })
+            
+        });
+      }
+    let fecha1input = document.getElementById('cantDini');
+    let fecha2input = document.getElementById('cantDfin');
+
+    let fechaActual = new Date();
+    let dia = fechaActual.getDate().toString().padStart(2, '0');
+    let mes = (fechaActual.getMonth() + 1).toString().padStart(2, '0');
+    let anio = fechaActual.getFullYear().toString();
+    
+    let fechaFinal1 = `${anio}-${mes}-${dia}`;
+    fecha1input.setAttribute('min', fechaFinal1);
+    fecha1input.addEventListener("change",()=>{
+        fecha2input.removeAttribute("disabled");
+        fecha2input.setAttribute('min', fecha1input.value);
+        fecha2input.addEventListener("change",()=>{
+            let fecha1 = new Date(fecha1input.value);
+            let fecha2 = new Date(fecha2input.value);
+            let diferenciaEnMilisegundos = Math.abs(fecha2.getTime() - fecha1.getTime());
+            let diasEntreFechas = Math.ceil(diferenciaEnMilisegundos / (1000 * 60 * 60 * 24));
+
+            console.log(diasEntreFechas);
+            
+            
+        });
+    });
+
+
+
+
 });
+
+
+
